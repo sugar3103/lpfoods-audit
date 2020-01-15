8f274a3dc0aed7d0a8cb5e0fb34bba1886cc4c62
Duong Phan <duong16628@gmail.com>
2020-01-15 17:15:13 +0700

    first time add project

---
+++ build/precache-manifest.c8806ad0e74e3f4e9ba3b328f6ed20c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f055d0a5876597e8262f363c473a26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ece7c333fb1f0362f43",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "299b329b0f39f67debc2",
+    "url": "/static/js/2.ca24d239.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ca24d239.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ece7c333fb1f0362f43",
+    "url": "/static/js/main.91c461d7.chunk.js"
+  },
+  {
+    "revision": "87c9893fa030c26502a4",
+    "url": "/static/js/runtime-main.f54e3186.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
